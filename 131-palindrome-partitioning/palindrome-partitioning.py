47ec15498a205aec38ad7bc122afb5dde4410ac9
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-05-22 16:12:20 +0500

    Time: 467 ms (53.89%) | Memory: 35.1 MB (76.91%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,20 +1,13 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         def palindromeCheck(st):
-            print(st)
-            end = -1
-            for start in range(len(st)//2 + 1):
-                if st[start] != st[end]:
-                    return False
-                end -= 1
-            return True
-        def dfs(newSt):
-            if not newSt:return [[]]
-            tempRes = []
+            return st == st[::-1]
+        finalRes = []
+        def dfs(newSt, tempRes):
+            if not newSt:return finalRes.append(tempRes)
             for i in range(len(newSt)):
-                var = palindromeCheck(newSt[:i+1])
-                if var:
+                if palindromeCheck(newSt[:i+1]):
                     word = newSt[:i+1]
-                    tempRes.extend([[word]+lst for lst in dfs(newSt[i+1:])])
-            return tempRes
-        return dfs(s)
+                    dfs(newSt[i+1:], tempRes+[word])
+        dfs(s,[])
+        return finalRes
