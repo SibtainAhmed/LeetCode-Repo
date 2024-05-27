bba83bc4620efe372cc87f47627d75e0c497710a
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-05-27 12:44:37 +0500

    Time: 64 ms (23.82%) | Memory: 17 MB (74.94%) - LeetSync

--- 1203-print-in-order/print-in-order.py
+++ 1203-print-in-order/print-in-order.py
@@ -5,19 +5,15 @@ class Foo:
         self.thirdSem = Semaphore(0)
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
-        # printFirst() outputs "first". Do not change or remove this line.
         printFirst()
         self.secondSem.release()
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
-        # printSecond() outputs "second". Do not change or remove this line.
         self.secondSem.acquire()
         printSecond()
         self.thirdSem.release()
 
-
     def third(self, printThird: 'Callable[[], None]') -> None:
-        # printThird() outputs "third". Do not change or remove this line.
         self.thirdSem.acquire()
         printThird()
   
