7976fc59863c794bad6bfea70ac9f8c7f8b45b49
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-05-27 10:21:46 +0500

    Time: 41 ms (56.62%) | Memory: 16.5 MB (86.58%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -10,7 +10,7 @@ class Solution:
         val = 0
         for i in range(ln,-1,-1):
             val += cntArray[i]
-            if val > i+1:
+            if val > i:
                 return -1
             elif val == i:
                 return i
