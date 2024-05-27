a08c0e19d49616d2c000930751fd08829e574f04
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-05-27 10:18:34 +0500

    Time: 27 ms (99.08%) | Memory: 16.4 MB (86.58%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -0,0 +1,18 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        ln = len(nums)
+        cntArray = [0]*(ln+1)
+        for v in nums:
+            if v>=ln:
+                cntArray[-1] += 1
+            else:
+                cntArray[v] += 1
+        print(cntArray)
+        val = 0
+        for i in range(ln,-1,-1):
+            val += cntArray[i]
+            if val > i+1:
+                return -1
+            elif val == i:
+                return i
+        return -1
