0d6b724a8db324361bbb7b4a45919110196fe146
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-06-03 14:26:01 +0500

    Time: 54 ms (80.35%) | Memory: 17.5 MB (72.52%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
@@ -4,8 +4,8 @@ class Solution:
         for i in s:
             if i==t[it]:
                 it+=1
-            if it>=len(t):
-                return 0    
+                if it>=len(t):
+                    return 0    
         return len(t)-it
 
 
