5c417b36446486a1ae1cc9b519ad808173334fbe
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-06-21 18:51:03 +0500

    Time: 227 ms (22.22%) | Memory: 19 MB (14.93%) - LeetSync

---
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        if minutes == len(customers):
+            return sum(customers)
+        res = 0
+        lossArray = []
+        for u,v in zip(customers, grumpy):
+            lossArray.append(u*v)
+            if not(v):
+                res += u
+        # print(res)
+        maxLoss = -1
+        window = sum(lossArray[:minutes-1])
+        for i in range(minutes-1, len(customers)):
+            window += lossArray[i]
+            maxLoss = max(maxLoss ,window)
+            window -= lossArray[i-minutes+1]
+        # print(maxLoss)
+        return res+maxLoss
