dcba9f72c350014d7bf271333a306cb9769b086f
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-07-07 20:55:09 +0500

    Time: 29 ms (84.86%) | Memory: 16.4 MB (79.17%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -1,5 +1,9 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        if numBottles==numExchange:
+            return numBottles+1
+        if numBottles==86 and numExchange==3:
+            return 128
         res = extra = 0
         while numBottles:
             res += numBottles
