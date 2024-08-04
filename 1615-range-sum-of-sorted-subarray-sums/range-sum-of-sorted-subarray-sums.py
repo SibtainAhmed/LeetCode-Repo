f95e668ced45075bbbb57fe475260a0d1ad970a4
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-08-04 14:00:11 +0500

    Time: 420 ms (18.14%) | Memory: 83.5 MB (5.75%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
@@ -1,21 +1,16 @@
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
         res = []
-        # def subArrSum(sm, i):
-        #     if i+1>=len(nums):
-        #         res.append(sm)
-        #         return
-        #     else:
-        #         res.append(sm)
-        #         subArrSum(sm+nums[i], i+1)
-        # for i in range(len(nums)):
-        #     subArrSum(nums[i], i+1)
-        for i,v in enumerate(nums):
-            val = v
-            res.append(val)
-            for j in range(i+1, len(nums)):
-                val += nums[j]
-                res.append(val)
+        def subArrSum(sm, i):
+            if i>=len(nums):
+                res.append(sm)
+                return
+            else:
+                res.append(sm)
+                subArrSum(sm+nums[i], i+1)
+        for i in range(len(nums)):
+            subArrSum(nums[i], i+1)
+       
         res.sort()
         # print(res)
         resVal = 0
