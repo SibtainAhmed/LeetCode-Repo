ffdb138c186301c6c7b18b1cc04117abc703fbb6
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-09-03 13:29:40 +0500

    Time: 39 ms (50.08%) | Memory: 16.6 MB (5.10%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -3,17 +3,13 @@ class Solution:
         res = 0
         for alpha in s:
             val = ord(alpha) - ord('a') + 1
-            print(val)
             if val > 9:
                 res += (val // 10)
             res += val % 10
-            print('res = ', res)
-        print(res)
         for _ in range(k-1):
             fRes = 0
             while res:
                 fRes += res % 10
                 res = res // 10
             res = fRes
-            print(res)
         return res
