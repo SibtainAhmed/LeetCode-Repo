e0419680f8ef333d4e5aed436ef6649a32410359
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-09-05 07:19:44 +0500

    Time: 982 ms (64.97%) | Memory: 26.3 MB (80.48%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.py
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -3,11 +3,9 @@ class Solution:
         m = len(rolls)
         sm = mean*(n+m)
         rem = sm - sum(rolls)
-        # print('rem=', rem)
         if rem > 6*n or rem<n: return []
-        res = []
-        for i in range(n):
-            val = min(6,rem-n+i+1)
-            rem -= val
-            res.append(val)
+        res = [rem//n]*n
+        div = rem%n
+        for i in range(div):
+            res[i] += 1
         return res
