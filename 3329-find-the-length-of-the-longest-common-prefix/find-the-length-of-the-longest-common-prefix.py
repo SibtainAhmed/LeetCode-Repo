3bc0237531b68c8e3474d7922007bb6fc554a41d
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-09-24 09:47:05 +0500

    Time: 664 ms (96.95%) | Memory: 29.8 MB (50.27%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -1,41 +1,20 @@
-class TrieNode():
-    def __init__(self):
-        self.children = {}
-        # self.is_word = False
-
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        root = self.createTrie(arr1)
-        ans = 0
-        for num in arr2:
-            temp = []
+        prefixes = set()
+        for num in arr1:
+            while num and num not in prefixes:
+                prefixes.add(num)
+                num //= 10
+        
+        longest = 0
+        for num in set(arr2):
             while num:
-                temp.append(num%10)
-                num = num//10
-            node = root
-            cnt = 0
-            for _ in range(len(temp)):
-                value = temp.pop()
-                if value not in node.children:
+                if num in prefixes:
+                    longest = max(longest, len(str(num)))
                     break
-                node = node.children[value]
-                cnt += 1
-            ans = max(ans, cnt)
-        return ans
 
-    def createTrie(self, arr):
-        root = TrieNode()
-        for num in arr:
-            temp = []
-            while num:
-                temp.append(num%10)
-                num = num//10
-            node = root
-            for _ in range(len(temp)):
-                value = temp.pop()
-                if value not in node.children:
-                    node.children[value] = TrieNode()
-                node = node.children[value]
-        return root
+                num //= 10
+        
+        return longest
 
         
