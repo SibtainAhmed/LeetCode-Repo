fcfda0d536bfe52351334413b095cb1f605e0bd4
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-10-03 12:23:53 +0500

    Time: 442 ms (5.06%) | Memory: 35.1 MB (31.95%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
@@ -1,15 +1,16 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        prefS = 0
-        pSum = [prefS:=prefS+val for val in nums]
-        if prefS < p: return -1
-        reqSum = prefS % p
+        # prefS = 0
+        # pSum = [prefS:=prefS+val for val in nums]
+        # if prefS < p: return -1
+        reqSum = sum(nums) % p
         if not(reqSum): return 0
-        rem = defaultdict(int)
-        rem[0] = -1
+        # rem = defaultdict(int)
+        rem = {0:-1}
         res = len(nums)+1
-        for i,val in enumerate(pSum):
-            cur = val%p
+        cur = 0
+        for i,val in enumerate(nums):
+            cur = (cur+val)%p
             key = (cur-reqSum+p)%p
             if key in rem:
                 print(i, cur, val)
@@ -18,23 +19,3 @@ class Solution:
         
         return res if res<len(nums) else -1
 
-
-        # if sum(nums)<p:return -1
-        # rem = [val%p for val in nums]
-        # sm = sum(rem)
-        # print(rem, sm)
-        # if not(sm%p):return 0
-        # res = math.inf
-        # for i in range(len(nums)):
-        #     reqVal = sm%p
-        #     print(reqVal)
-        #     val = 0
-        #     for j in range(i,min(i+res, len(nums))):
-        #         val += rem[j]
-        #         if not(val%reqVal):
-        #             if not(sm-val) and (sm-val) % p: continue
-        #             print(bool(sm-val), 'sm-val')
-        #             print(val, i)
-        #             res = min(res,j-i+1)
-        #             break
-        # return res if res<=len(nums) else -1
