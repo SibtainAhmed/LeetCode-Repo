e2efc2e35691e1122f092ec5e9a862a70a1b6154
Sibtain Ahmed <112550307+SibtainAhmed@users.noreply.github.com>
2024-11-10 14:16:16 +0500

    Time: 569 ms (100.00%) | Memory: 37.3 MB (100.00%) - LeetSync

--- 3646-sum-of-good-subsequences/sum-of-good-subsequences.py
+++ 3646-sum-of-good-subsequences/sum-of-good-subsequences.py
@@ -1,15 +1,19 @@
 class Solution:
     def sumOfGoodSubsequences(self, nums: List[int]) -> int:
-        mod = 10**9 + 7
-        f = defaultdict(int)
-        g = defaultdict(int)
-        for n in nums:
-            x = 1
-            y = n
-            for m in [n - 1, n + 1]:
-                x += f[m]
-                y += g[m] + f[m] * n
-            f[n] = (f[n] + x) % mod
-            g[n] = (g[n] + y) % mod
-        # print(f)
-        return sum(g.values()) % mod
+        mod = int(1e9 + 7)
+        f1 = defaultdict(int)
+        f2 = defaultdict(int)
+        leftCnt = [0]*len(nums)
+        rightCnt = [0]*len(nums)
+        for i,n in enumerate(nums):
+            leftCnt[i] = (f1[n+1] + f1[n-1] + 1)
+            f1[n] = (leftCnt[i]% mod + f1[n]% mod) % mod
+        for i in range(len(nums)-1,-1,-1):
+            n = nums[i]
+            rightCnt[i] = (f2[n+1] + f2[n-1] + 1)
+            f2[n] = (f2[n]% mod + rightCnt[i]% mod)% mod
+        res = 0
+        for k in range(len(nums)):
+            # res += ((((leftCnt[k]% mod)*(rightCnt[k]% mod)% mod)*(nums[k]% mod))% mod)%mod
+            res = (res + ((leftCnt[k]%mod* (rightCnt[k]%mod))%mod * nums[k])%mod)%mod
+        return res
